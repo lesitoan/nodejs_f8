485dd04b90ba40f4c82f93ca4ff1da1a0041346d
lesitoan <lesitoan.media@gmail.com>
2023-04-01 22:21:07 +0700

    finishLearn

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // template engine
 app.engine('hbs', handlebars({
@@ -22,6 +26,17 @@ app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body)
+
+    res.send('');
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
